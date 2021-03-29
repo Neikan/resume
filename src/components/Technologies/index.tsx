6910294d7d346b5481a705a5f09c81d130fb0aad
Neikan <sladkov.e.m@outlook.com>
2021-03-29 20:53:35 +0300

    feat(Technologies): update icons

--- src/components/Technologies/index.tsx
+++ src/components/Technologies/index.tsx
@@ -1,4 +1,5 @@
-import React, { FC, ReactElement } from 'react'
+import cn from 'classnames'
+import React, { FC } from 'react'
 
 import { IRCLIconProps } from '@neikan/rc-lib'
 import {
@@ -30,49 +31,57 @@ import {
   IconZeplin
 } from '@components/icons'
 
+import styles from './Technologies.module.css'
+
 const iconProps: IRCLIconProps = {
-  size: 'x3',
+  size: 'x2h',
   classes: 'grid-item'
 }
-
-const technologies = [
-  <IconReact key='' { ...iconProps } />,
-  <IconRedux key='' { ...iconProps } />,
-  <IconSASS key='' { ...iconProps } />,
-  <IconSwagger key='' { ...iconProps } />,
-  <IconStorybook key='' { ...iconProps } />,
-  <IconStyleguidist key='' { ...iconProps } />,
-  <IconCordova key='' { ...iconProps } />,
-  <IconCSS key='' { ...iconProps } />,
-  <IconEnzyme key='' { ...iconProps } />,
-  <IconFigma key='' { ...iconProps } />,
-  <IconTS key='' { ...iconProps } />,
-  <IconWebStorm key='' { ...iconProps } />,
-  <IconGit key='' { ...iconProps } />,
-  <IconGulp key='' { ...iconProps } />,
-  <IconHTML key='' { ...iconProps } />,
-  <IconJest key='' { ...iconProps } />,
-  <IconWebpack key='' { ...iconProps } />,
-  <IconReactRouter key='' { ...iconProps } />,
-  <IconJS key='' { ...iconProps } />,
-  <IconLess key='' { ...iconProps } />,
-  <IconPhotoshop key='' { ...iconProps } />,
-  <IconPostman key='' { ...iconProps } />,
-  <IconZeplin key='' { ...iconProps } />,
-  <IconVSCode key='' { ...iconProps } />,
-  <IconBabel key='' { ...iconProps } />,
-  <IconSQL key='' { ...iconProps } />
-]
-
-const renderIcon = (icon: ReactElement): ReactElement => <>{icon}</>
 
 // Добавить джиру, бейскемп, тфс
 export const Technologies: FC = () => {
   return (
     <>
-      <h2>Technologies</h2>
-      <div className='grid-5 grid-g-4 grid-center'>
-        {technologies.map(renderIcon)}
+      <h3>Technologies and skills</h3>
+      <div className={cn(styles.wrapper, 'grid-4 grid-g-4 grid-center mb-4')}>
+        <IconTS { ...iconProps } />
+        <IconJS { ...iconProps } />
+        <IconReact { ...iconProps } />
+        <IconReactRouter { ...iconProps } />
+        <IconRedux { ...iconProps } />
+        <IconCordova { ...iconProps } />
+        <IconGit { ...iconProps } />
+        <IconSQL { ...iconProps } />
+      </div>
+      <div className={cn(styles.wrapper, 'grid-4 grid-g-4 grid-center mb-4')}>
+        <IconHTML { ...iconProps } />
+        <IconCSS { ...iconProps } />
+        <IconLess { ...iconProps } />
+        <IconSASS { ...iconProps } />
+      </div>
+      <div className={cn(styles.wrapper, 'grid-4 grid-g-4 grid-center mb-4')}>
+        <IconFigma { ...iconProps } />
+        <IconPhotoshop { ...iconProps } />
+        <IconZeplin { ...iconProps } />
+      </div>
+      <div className={cn(styles.wrapper, 'grid-4 grid-g-4 grid-center mb-4')}>
+        <IconSwagger { ...iconProps } />
+        <IconPostman { ...iconProps } />
+        <IconEnzyme { ...iconProps } />
+        <IconJest { ...iconProps } />
+      </div>
+      <div className={cn(styles.wrapper, 'grid-4 grid-g-4 grid-center mb-4')}>
+        <IconWebpack { ...iconProps } />
+        <IconGulp { ...iconProps } />
+        <IconBabel { ...iconProps } />
+      </div>
+      <div className={cn(styles.wrapper, 'grid-4 grid-g-4 grid-center mb-4')}>
+        <IconStorybook { ...iconProps } />
+        <IconStyleguidist { ...iconProps } />
+      </div>
+      <div className={cn(styles.wrapper, 'grid-4 grid-g-4 grid-center mb-4')}>
+        <IconVSCode { ...iconProps } />
+        <IconWebStorm { ...iconProps } />
       </div>
     </>
   )
