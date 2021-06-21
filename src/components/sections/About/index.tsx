d961ce8c114ac651f519e2fbc0d603ccb7e210fe
Neikan <sladkov.e.m@outlook.com>
2021-06-21 21:12:54 +0300

    feat(project): update components

--- src/components/sections/About/index.tsx
+++ src/components/sections/About/index.tsx
@@ -5,6 +5,7 @@ import { Texts } from './components/Texts'
 
 export const About: FC = () => (
   <section className='about'>
+    <h3 className='visually-hidden'>About</h3>
     <Photo />
     <Texts />
   </section>
--- webpack/alias.js
+++ webpack/alias.js
@@ -5,9 +5,7 @@ const defaultPath = path.resolve(__dirname, '../src');
 const paths = {
   '@components': path.resolve(defaultPath, 'components'),
   '@consts': path.resolve(defaultPath, 'consts'),
-  '@types': path.resolve(defaultPath, 'types'),
-  '@utils': path.resolve(defaultPath, 'utils'),
-  '@views': path.resolve(defaultPath, 'views')
+  '@types': path.resolve(defaultPath, 'types')
 };
 
 module.exports = paths;
